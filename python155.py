115a4417b8ed2ded5aadeb4179955239866da296
Mobeen Ahmad <ahmadmobeen24@gmail.com>
2022-05-30 13:45:58 +0900

    Update python155.py

--- python155.py
+++ python155.py
@@ -1,19 +1,68 @@
 class MinStack:
-    # Simple solution with using Python List
+
     def __init__(self):
         self.stack = []
+        self.minArr = []
 
     def push(self, val: int) -> None:
         self.stack.append(val)
+        
+        if len(self.minArr) == 0:
+            self.minArr.append(val)
+        else:
+            if self.minArr[-1] > val:
+                self.minArr.append(val)
+            else:
+                tmp_minArr = []
+                while len(self.minArr) !=0 and val > self.minArr[-1]:   ## Empty the minArr in a temp list until the new value is higher than the
+                                                                        ## minArr top    
+                    tmp_minArr.append(self.minArr.pop())     
 
+                self.minArr.append(val)                                 ## Append the new value in minArr
+                while len(tmp_minArr) != 0:                         
+                    self.minArr.append(tmp_minArr.pop())                ## Empty the empty array back in minArr
+        
     def pop(self) -> None:
-        self.stack.pop()
+        val = self.stack.pop()
+        
+        for i in range(len(self.minArr)):   #### Find the location of the self.stack.pop value in minArr
+            if val == self.minArr[i]:
+                while i < len(self.minArr)-1:
+                    self.minArr[i] = self.minArr[i+1]
+                    i += 1
+                break
+        
+        ### Remove the self.stack.pop value from minArr
+        self.minArr.pop()
+                
+        
 
     def top(self) -> int:
         return self.stack[-1]
 
     def getMin(self) -> int:
-        return min(self.stack)
+        # return min(self.stack) # O(n)
+        return self.minArr[-1]
+        
+
+
+
+# class MinStack:
+#     # Simple solution with using Python List
+#     def __init__(self):
+#         self.stack = []
+
+#     def push(self, val: int) -> None:
+#         self.stack.append(val)
+
+#     def pop(self) -> None:
+#         self.stack.pop()
+
+#     def top(self) -> int:
+#         return self.stack[-1]
+
+#     def getMin(self) -> int:
+#         return min(self.stack)
 
 
 # Your MinStack object will be instantiated and called as such:
